bd70d865b3859941b205feeb063e970f5d1ac77c
qeg1215 <kjhnml1234@naver.com>
2022-10-01 13:14:02 +0900

    first commit

---
+++ src/main/java/project/loa/loapage/LoapageApplication.java
@@ -0,0 +1,13 @@
+package project.loa.loapage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoapageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoapageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/project/loa/loapage/LoapageApplicationTests.java
@@ -0,0 +1,13 @@
+package project.loa.loapage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoapageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
